aa4a7a90861d5efe69493b786b5f4ea6c2039a0d
Tudor1977 <teo23bocaneala@gmail.com>
2025-06-18 10:58:07 +0300

    Add files via upload

---
+++ site-final-vercel-ready/firebase-config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQG6WD-N7ZjtNOC_zf8kMASMYEDujihdM",
+  authDomain: "cabinet-psihologic-4666e.firebaseapp.com",
+  databaseURL: "https://cabinet-psihologic-4666e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cabinet-psihologic-4666e",
+  storageBucket: "cabinet-psihologic-4666e.firebasestorage.app",
+  messagingSenderId: "46348242698",
+  appId: "1:46348242698:web:15759be5ce3d4c4df274f3"
+};
